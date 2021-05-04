8965e921a4fb5dd3c903e3ba7328287c3f5b8be6
Rui Martins <rui@skarp.dk>
2021-05-04 19:34:46 +0200

    designing ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 pt-5">
+            <img src="https://toogoodtogo.dk/v2020/front-page/1-3-bowl.png" style="height: 200px;" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5 pl-5">
+        <div><h1>Rui TGTG</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">Rui TGTG</div>
+            <div>An introduction to Laravel PHP Framework.</div>
+            <div><a href="https://github.com/MrRuiMartins/rui-tgtg">github.com/MrRuiMartins/rui-tgtg</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://toogoodtogo.dk/v2020/front-page/floating-food-bag.png" class="w-100 h-100 pt-4">
+        </div>
+        <div class="col-4">
+            <img src="https://toogoodtogo.dk/v2020/front-page/give-bag.png" class="w-100 h-100 pt-4">
+        </div>
+        <div class="col-4">
+            <img src="https://toogoodtogo.dk/v2020/front-page/food-basket.png" class="w-100 h-100 pt-4">
         </div>
     </div>
 </div>
